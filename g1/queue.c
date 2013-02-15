54c6d6b1c94bc2e8e53b98bb89567171d7a27fd7
Jenny <jennyvej@gmail.com>
2013-02-15 17:41:07 +0100

    fixed stuff

--- g1/queue.c
+++ g1/queue.c
@@ -2,12 +2,13 @@
 #include <stdio.h>
 #include "queue.h"
 
+/* return number of elements in queue */
 int length(QNode* queue) {
-    int theLength = 0;
-    QNode* current = queue->link;
+  int theLength = 0; 
     if (queue != NULL) { //There is at least 1 element
-        ++theLength;
-        while (queue != current) { //While there are more different elements
+      QNode* current = queue;  
+      ++theLength;
+        while (queue != current->link) { //While there are more different elements
             ++theLength;
             current = current->link;
         }
@@ -15,6 +16,7 @@ int length(QNode* queue) {
     return theLength;
 }
 
+/* add element at rear end */
 void enqueue(QNode** queue, Data el) {
     QNode *QNode;
     QNode = malloc( sizeof(QNode) );
@@ -22,25 +24,43 @@ void enqueue(QNode** queue, Data el) {
         printf("Out of memory");
         exit(0);
     }
-    QNode->content = el;
-    if (*queue == NULL) 
+    QNode->content = el; 
+    if (*queue == NULL) //if queue is empty 
         QNode->link = QNode;
-    else
+    else { 
         QNode->link = (*queue)->link;
+	(*queue)->link = QNode;
+    } 
     *queue = QNode;
 }
 
+/* remove and return front element */
 Data dequeue(QNode** queue) {
     QNode* current = (*queue)->link;
     Data el = current->content;
-    if ((*queue)->link == current->link)
-        (*queue)->link = NULL;
-    else 
-        (*queue)->link = current->link;
+    if ((*queue)->link == current->link) {
+      *queue = NULL;
+    }
+    else { 
+      (*queue)->link = current->link;
+    }
     free(current);
     return el;
 }
 
-int main (){
-    return 0;
+int main () {
+  QNode* testNode = NULL;
+  printf("laengde =%d\n", length(testNode));
+  enqueue (&testNode, "esjlf");
+  printf("laengde =%d\n", length(testNode));
+  enqueue (&testNode, "akfhdfkgjh");
+  printf("laengde =%d\n", length(testNode));
+
+  printf("vaerdi =%s\n", (char*) dequeue(&testNode));
+  printf("laengde =%d\n", length(testNode));
+  printf("vaerdi =%s\n", (char*) dequeue(&testNode));
+  printf("laengde =%d\n", length(testNode));
+
+  
+  return 0;
 }
