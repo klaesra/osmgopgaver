6daad50f24f3e22ea5c9aef290f40f228b2d122d
Klaes Rasmussen <k@klaes.dk>
2013-03-05 00:26:15 +0100

    Adding what we have so far on g3

--- buenos/kernel/lock_cond.c
+++ buenos/kernel/lock_cond.c
@@ -1,2 +1,73 @@
 #include "kernel/lock_cond.h"
+#include "kernel/interrupt.h"
+#include "kernel/sleepq.h"
+#include "lib/libc.h"
 
+static lock_t lock_table[LOCK_COND_MAX_LOCKS];
+
+int lock_reset( lock_t *lock ){
+    spinlock_reset(&lock->slock);
+    lock->locked = -1;
+    return 0;
+}
+
+
+void lock_acquire( lock_t *lock ) {
+    interrupt_status_t intr_status;
+
+    intr_status = _interrupt_disable();
+    spinlock_acquire(&lock->slock);
+
+    while (lock->locked >= 0) {
+        sleepq_add(lock);
+        spinlock_release(&lock->slock);
+        thread_switch();
+    }
+    lock->locked = 0;
+    
+    spinlock_release(&lock->slock);
+    _interrupt_set_state(intr_status);
+    
+}
+
+void lock_release( lock_t *lock ) {
+    interrupt_status_t intr_status;
+    
+    intr_status = _interrupt_disable();
+    spinlock_acquire(&lock->slock);
+
+    if (lock->locked < 0) {
+        sleepq_wake(lock);
+    }
+
+    spinlock_release(&lock->slock);
+    _interrupt_set_state(intr_status);
+}
+
+
+void condition_init (cond_t *cond) {
+    int i;
+    if (cond) {
+        for (i = 0; i < LOCK_COND_MAX_LOCKS ; i++) {
+            lock_reset(&lock_table[i]);
+        }
+    }
+}
+
+void condition_wait (cond_t *cond, lock_t *lock ) {
+    if (cond) {
+        lock_acquire(lock);
+    }
+}
+
+void condition_signal (cond_t *cond, lock_t *lock ) {
+    if (cond) {
+        lock_release(lock);
+    }
+}
+
+void condition_broadcast (cond_t *cond, lock_t *lock ) {
+    if (cond) {
+        sleepq_wake_all(lock);
+    }
+}
--- buenos/kernel/lock_cond.h
+++ buenos/kernel/lock_cond.h
@@ -1,16 +1,28 @@
 #ifndef BUENOS_KERNEL_LOCK_H
 #define BUENOS_KERNEL_LOCK_H
 
-typedef int lock_t;
+#include "kernel/spinlock.h"
+#include "kernel/thread.h"
+
+typedef int cond_t;
+
+typedef struct {
+    spinlock_t slock;
+    int value;
+    TID_t locked;
+} lock_t;
+
+#define LOCK_COND_MAX_LOCKS 32
+
 
 int lock_reset(lock_t *lock);
 void lock_acquire(lock_t *lock);
 void lock_release(lock_t *lock);
 
-void condition_init (cond t *cond);
-void condition_wait (cond t *cond, lock t *lock );
-void condition_signal (cond t *cond, lock t *lock );
-void condition_broadcast (cond t *cond, lock t *lock );
+void condition_init (cond_t *cond);
+void condition_wait (cond_t *cond, lock_t *lock );
+void condition_signal (cond_t *cond, lock_t *lock );
+void condition_broadcast (cond_t *cond, lock_t *lock );
 
 
 #endif /* BUENOS_KERNEL_LOCK_H */
